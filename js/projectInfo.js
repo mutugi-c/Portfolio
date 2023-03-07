6115548b470ef9636120764658957527b4d48dd0
mutugi-c <mutugichris@gmail.com>
2023-03-07 20:58:46 +0100

    Add array to store projects' information

---
+++ js/projectInfo.js
@@ -0,0 +1,39 @@
+const projectInfo = [
+    {
+        name: "Geocoding app",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum tempora voluptatum esse blanditiis, eligendi harum nulla reiciendis excepturi quod.",
+        featuredImage: "./images/img1.jpg",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveVersion: "https://landing-page-skilled-elearning.netlify.app/",
+        source: "https://github.com/mutugi-c/Skilled-elearning-landing-page"
+    },
+
+    {
+        name: "Geocoding app",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum tempora voluptatum esse blanditiis, eligendi harum nulla reiciendis excepturi quod.",
+        featuredImage: "./images/img3.jpg",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveVersion: "https://statistics-preview-card.netlify.app/",
+        source: "https://github.com/mutugi-c/Stats-preview-card"
+    },
+
+    {
+        name: "Geocoding app",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum tempora voluptatum esse blanditiis, eligendi harum nulla reiciendis excepturi quod.",
+        featuredImage: "./images/img2.jpg",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveVersion: "https://preview-card-component-product.netlify.app/",
+        source: "https://github.com/mutugi-c/Product-preview-card-component"
+    },
+
+    {
+        name: "Geocoding app",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum tempora voluptatum esse blanditiis, eligendi harum nulla reiciendis excepturi quod.",
+        featuredImage: "./images/img4.jpg",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        liveVersion: "https://main-qr-code-component.netlify.app/",
+        source: "https://github.com/mutugi-c/QR-code-component"
+    }
+]
+
+export default projectInfo;
