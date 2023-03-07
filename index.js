225ef01e4a11cbbf0beebde31882edc5635a08c1
mutugi-c <mutugichris@gmail.com>
2023-03-07 16:29:35 +0100

    Rectify linters' errors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const hamburgerIcon = document.querySelector(".hamburger-icon");
-const pageMenu = document.querySelector(".nav__list");
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const pageMenu = document.querySelector('.nav__list');
 
-hamburgerIcon.addEventListener("click", () => {
-    hamburgerIcon.classList.toggle("active");
-    pageMenu.classList.toggle("active");
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.classList.toggle('active');
+  pageMenu.classList.toggle('active');
 });
 
-pageMenu.addEventListener("click", () => {
-    hamburgerIcon.classList.remove("active");
-    pageMenu.classList.remove("active");
+pageMenu.addEventListener('click', () => {
+  hamburgerIcon.classList.remove('active');
+  pageMenu.classList.remove('active');
 });
