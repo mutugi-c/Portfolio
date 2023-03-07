2a563db4b4ad7690eeed0c1207b27fc5defa14d0
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-07 09:13:57 -0600

    Add event listener

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const pageMenu = document.querySelector(".nav__list");
+
+hamburgerIcon.addEventListener("click", () => {
+    hamburgerIcon.classList.toggle("active");
+    pageMenu.classList.toggle("active");
+});
+
