a4f328c4b37d007dd7cb72798383b73a80e233fb
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-08 10:22:19 -0600

    Add logic to create the popup menu

--- js/projects.js
+++ js/projects.js
@@ -47,3 +47,61 @@ for (let i = 0; i <= projectInfo.length; i ++) {
       document.querySelectorAll('.card')[i].classList.add('order');
     }
   }
+
+  //   Popup
+document.querySelector("#popups").innerHTML = projectInfo.map(
+    (info) => `
+    <div class="popup-container">
+      <div class="popup">
+        <div class="card__body popup--body">
+          <div class="popup--header">
+            <h2 class="card__heading popup--heading">${info.name}</h2>
+            <img class="popup__exit" src="images/x_icon.svg" alt="" />
+          </div>
+
+          <div class="card__details">
+            <h4>${info.author}</h4>
+            <div>
+              <ul class="card__list">
+                <li class="card__role">${info.stack}</li>
+                <li class="card__year">${info.year}</li>
+              </ul>
+            </div>
+          </div>
+
+          <img
+            class="card__image popup--img"
+            src=${info.featuredImage}
+            alt="Tonic project image with the heading 'GYMFIT', and a lady doing yoga stretching"
+          />
+          <img
+            class="card__image-desktop popup--img"
+            src=${info.featuredImage}
+            alt="Desktop image version for the Tonic project indicating 'From high school dropout to product developer at Facebook and Lyft: Tanner Christensen'"
+          />
+
+          <div class="popup--info">
+            <p class="card__text">
+            ${info.description}
+            </p>
+            <div class="tags-buttons">
+              <div class="card__languages popup--tags">
+                <button class="btn--primary">${info.technologies[0]}</button>
+                <button class="btn--primary">${info.technologies[1]}</button>
+                <button class="btn--primary">${info.technologies[2]}</button>
+              </div>
+              <div class="card__btn popup--buttons">
+                <a href="${info.liveVersion}" class="btn--secondary popup--btn" target="_blank" rel="noopener noreferrer"
+                  >See Live <img src="images/live.svg" alt=""
+                /></a>
+                <a href="${info.source}" class="btn--secondary popup--btn" target="_blank" rel="noopener noreferrer"
+                  >See Source <img src="images/github_blue.svg" alt=""
+                /></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `
+).join('');
