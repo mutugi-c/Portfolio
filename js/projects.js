de7bb99e0a3fc0dccd6d66d798e15a8fa104cdd5
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-08 10:20:10 -0600

    Add structure to create html dynamically

---
+++ js/projects.js
@@ -0,0 +1,43 @@
+import projectInfo from "./projectInfo.js";
+
+document.querySelector('#works').innerHTML = projectInfo.map(
+    (info) => `
+        <!--card  -->
+        <div class="card">
+          <img
+            class="card__image"
+            src=${info.featuredImage}
+            alt="Image for the tonic project indicating 'From high school dropout to product developer at Facebook and Lyft: Tanner Christensen'"
+          />
+          <img
+            class="card__image-desktop"
+            src="images/project1_desktop.png"
+            alt="Desktop version image for Nature project indicating 'Catchy look inspiring bionic forms $35'"
+          />
+          <div class="card__body">
+            <h2 class="card__heading">${info.name}</h2>
+            <div class="card__details">
+              <h4>${info.author}</h4>
+              <div>
+                <ul class="card__list">
+                  <li class="card__role">${info.stack}</li>
+                  <li class="card__year">${info.year}</li>
+                </ul>
+              </div>
+            </div>
+            <p class="card__text">
+             ${info.description}
+            </p>
+            <div class="card__languages">
+              <button class="btn--primary">${info.technologies[0]}</button>
+              <button class="btn--primary">${info.technologies[1]}</button>
+              <button class="btn--primary">${info.technologies[2]}</button>
+            </div>
+            <div class="card__btn-wrapper">
+            <button type="button" class="btn--secondary card__btn">See Project</button>
+            </div>
+          </div>
+        </div>
+        `
+)
+.join('');
