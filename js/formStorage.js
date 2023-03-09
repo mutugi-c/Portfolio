6eb8bd5a7f2fba205942c5afc8bcf439c920afc4
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-09 13:33:39 -0600

    Obtain data from local storage

--- js/formStorage.js
+++ js/formStorage.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const formData = {
-      name: '',
-      email: '',
-      message: ''
+        name: '',
+        email: '',
+        message: ''
     };
-  
+
     const nameInput = document.getElementById('full_name');
     const emailInput = document.getElementById('user_email');
     const messageInput = document.getElementById('message');
+
+    // Obtain stored data from local storage if available
+    const storedData = localStorage.getItem('formData');
+    if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        formData.name = parsedData.name;
+        formData.email = parsedData.email;
+        formData.message = parsedData.message;
+    }
 });
