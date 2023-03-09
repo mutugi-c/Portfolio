e02ad39086f00b8d220eca83c973c738890c1060
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-09 13:32:38 -0600

    Create formData object

---
+++ js/formStorage.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formData = {
+      name: '',
+      email: '',
+      message: ''
+    };
+  
+    const nameInput = document.getElementById('full_name');
+    const emailInput = document.getElementById('user_email');
+    const messageInput = document.getElementById('message');
+});
