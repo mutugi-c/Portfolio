72e73106092c5cd5673e520a8b1a2d64ca6f00f9
mutugi-c <mutugichris@gmail.com>
2023-03-09 20:39:33 +0100

    Update formStorage.js

--- js/formStorage.js
+++ js/formStorage.js
@@ -1,36 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const formData = {
-        name: '',
-        email: '',
-        message: ''
-    };
+  const formData = {
+    name: '',
+    email: '',
+    message: ''
+  };
 
-    const nameInput = document.getElementById('full_name');
-    const emailInput = document.getElementById('user_email');
-    const messageInput = document.getElementById('message');
+  const nameInput = document.getElementById('full_name');
+  const emailInput = document.getElementById('user_email');
+  const messageInput = document.getElementById('message');
 
-    // Obtain stored data from local storage if available
-    const storedData = localStorage.getItem('formData');
-    if (storedData) {
-        const parsedData = JSON.parse(storedData);
-        formData.name = parsedData.name;
-        formData.email = parsedData.email;
-        formData.message = parsedData.message;
-    }
-    nameInput.value = formData.name;
-    emailInput.value = formData.email;
-    messageInput.value = formData.message;
+  // Obtain data from local storage if available
+  const storedData = localStorage.getItem('formData');
+  if (storedData) {
+    const parsedData = JSON.parse(storedData);
+    formData.name = parsedData.name;
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+  }
+  nameInput.value = formData.name;
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
 
-    // Store form data to local storage when a field changes
-    const storeFormData = () => {
-        formData.name = nameInput.value;
-        formData.email = emailInput.value;
-        formData.message = messageInput.value;
+  // Store form data to local storage when a field changes
+  const storeFormData = () => {
+    formData.name = nameInput.value;
+    formData.email = emailInput.value;
+    formData.message = messageInput.value;
 
-        localStorage.setItem('formData', JSON.stringify(formData));
-    }
+    localStorage.setItem('formData', JSON.stringify(formData));
+  }
 
-    nameInput.addEventListener('input', storeFormData);
-    emailInput.addEventListener('input', storeFormData);
-    messageInput.addEventListener('input', storeFormData);
+  nameInput.addEventListener('input', storeFormData);
+  emailInput.addEventListener('input', storeFormData);
+  messageInput.addEventListener('input', storeFormData);
 })
